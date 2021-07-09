107c6e97ebce29b27b180fc848a7b0fe72f5f24e
Sebastian Karlsson <ianertson@gmail.com>
2021-07-09 21:23:34 +0200

    change theme to gruvbox

--- config.el
+++ config.el
@@ -3,6 +3,8 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+(setq doom-theme 'doom-gruvbox)
+
 (require 'eslint-fix)
 
 (eval-after-load 'js2-mode
@@ -104,7 +106,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+; (setq doom-theme 'doom-one)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
